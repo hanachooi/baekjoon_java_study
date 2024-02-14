5ac1b16b98f5c1ac71a84a2bce087ee1dbace3e6
hanachooi <92570309+hanachooi@users.noreply.github.com>
2024-02-14 18:03:27 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver V/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		// int 형으로 받아야함. String 은 누적하는게 아니기 때문. 
+		int g = N;
+		
+		for(int i = 0; i<N; i++) {
+			String s = br.readLine();
+			boolean[] visited = new boolean[26];
+			visited[s.charAt(0)-'a'] = true;
+			for(int j = 1; j<s.length(); j++) {
+				if(visited[s.charAt(j) - 'a']) {
+					if( s.charAt(j-1) != s.charAt(j) ) {
+						g--;
+						break;
+					}
+				}
+				visited[s.charAt(j)-'a'] = true;
+				
+			}
+		}
+		System.out.println(g);
+	}
+}
