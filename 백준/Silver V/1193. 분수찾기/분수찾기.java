3c0643e35d5a0f81f64d46e4a51c56908c50612d
hanachooi <92570309+hanachooi@users.noreply.github.com>
2024-02-19 10:03:34 +0900

    [Silver V] Title: 분수찾기, Time: 180 ms, Memory: 16076 KB -BaekjoonHub

---
+++ 백준/Silver V/1193. 분수찾기/분수찾기.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{ 
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int a = 1;		// 분자
+		int b = 2;		// 분모
+		
+		if(N == 1) {
+			System.out.println("1/1");
+		}else if(N == 2) {
+			System.out.println(a + "/" + b);
+		}else {
+		
+			for(int i = 2; i<N; i++) {
+			
+				if(a == 1) {
+					if((a+b)%2 == 0) {
+						b++;
+						continue;
+					}
+					if((a+b) % 2 != 0) {
+						a++;
+						b--;
+						continue;
+					}
+							
+				}else if(b == 1) {
+					if((a+b) % 2 != 0) {
+						a++;
+						continue;
+					}else{
+						a--;
+						b++;
+
+						continue;
+					}
+				}else {
+					if((a+b) % 2 == 0 ) {
+						a--;
+						b++;
+					}else {
+						a++;
+						b--;
+					}
+				}
+				
+			}
+			
+			System.out.println(a + "/" + b);
+		}
+		
+		
+	}
+}
